2ab1f7a3433ae8d4178eb31877681fac131d293f
xiaoxy <xiaoxy@guahao.com>
2020-07-17 17:38:52 +0800

    fix: add

---
+++ vue/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    test
+    {{ count }} {{ obj.foo }}
+    <button @click="increment">count++</button>
+  </div>
+</template>
+
+<script>
+  import { ref, reactive, onMounted } from '@vue/composition-api'
+  export default {
+    setup() {
+      onMounted(() => {
+        console.log('mounted!')
+      })
+
+      const count = ref(0)
+
+      const obj = reactive({foo: 'bar'})
+
+      function increment () {
+        count.value++
+      }
+
+      return {
+        count,
+        obj,
+        increment
+      }
+    }
+  }
+</script>
